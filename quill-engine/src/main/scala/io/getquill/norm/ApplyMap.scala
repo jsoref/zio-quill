d1db4e95bb52d3463d3c992368bdb41b51164419
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-09-20 18:27:40 -0400

    spelling: gin
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- quill-engine/src/main/scala/io/getquill/norm/ApplyMap.scala
+++ quill-engine/src/main/scala/io/getquill/norm/ApplyMap.scala
@@ -10,7 +10,7 @@ import io.getquill.sql.Common.ContainsImpurities
  * Notes for the conceptual examples below. Gin and Tonic were used as prototypical
  * examples of things that "are joined". In the table form, they are aliased to the following
  * tonics is Query[Tonic], tonic is Tonic
- * gins is Query[Gin], is Gin
+ * gins is Query[Gin], gin is Gin
  * waters is Query[Water], water is Water
  *
  * ginifySpirit is some f:Spirit => Gin
